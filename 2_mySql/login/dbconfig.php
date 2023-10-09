1ecbf5a74bdffd5cb00bb4b258c896b6cb773b8a
wsun <git>
2023-10-08 20:26:05 -0400

    add php login example

---
+++ 2_mySql/login/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+$server_name = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ccrapp";
+
+$conn = new mysqli($server_name, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
---
+++ 2_mySql/login/login.php
@@ -0,0 +1,53 @@
+<?php require 'dbconfig.php' ?>
+<html>
+<body>
+<?php
+$isPost=($_SERVER["REQUEST_METHOD"] == "POST");
+?>
+
+<?php if (!$isPost): ?>
+    <!-- login form -->
+    <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+    UName: <input type="text" name="uname"><br>
+    Pwd: <input type="text" name="pwd"><br>
+    <input type="submit">
+    </form>
+
+
+<?php else:?>
+
+    <?php
+        //load username and password from post
+        $uname=$_POST["uname"];
+        $pwd=$_POST["pwd"];
+        $error="";
+
+        // validation
+        if(isset($uname) || empty($uname )){
+            $error= "username is required!";
+        }        
+        if(isset($pwd) || empty($pwd)){
+            $error= "password is required!";
+        }
+
+
+        if(!empty($error)){
+            echo '<p>'.$error.' <br>';
+        }else{
+            //to load db to check pwd
+            $sql = "SELECT * FROM `user` WHERE Email='{$uname}' and Password='{$pwd}';";
+            $result = $conn->query($sql);  
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $id=$row['Id'];
+                echo "<p>Welcome '.$uname.', your id is {$id} <br>";
+            }
+            
+        }  
+
+    ?>
+    <a href=""> back </a>
+<?php endif; ?>
+
+</body>
+</html>
